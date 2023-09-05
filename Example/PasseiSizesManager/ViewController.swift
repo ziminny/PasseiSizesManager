7876da925b64d3fb63ae67336572fd418831ed0f
Vagner Oliveira <ziminny@gmail.com>
2023-09-05 16:50:20 -0300

    Initial commit

---
+++ Example/PasseiSizesManager/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PasseiSizesManager
+//
+//  Created by 95707007 on 09/05/2023.
+//  Copyright (c) 2023 95707007. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
